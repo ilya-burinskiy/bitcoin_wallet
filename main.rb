8f134e8d8ef1dd029038d7f80890d32425ef4b48
Ilya Burinsky <burinskiy992@gmail.com>
2025-07-08 18:07:48 +0300

    send fixup

--- main.rb
+++ main.rb
@@ -40,29 +40,61 @@ def btc2satoshi(btc)
   (btc * 1e8).to_i
 end
 
-def send_(receiver_addr, btc_amount)
+def send_(to_addr, btc_amount)
   privkey = File.open('private_key', 'r') { |f| f.read }
-  pubkey = Bitcoin::Secp256k1::Ruby.generate_pubkey(privkey)
-  wpkh = Bitcoin::Descriptor::Wpkh.new(pubkey)
-  addr = wpkh.to_script.to_addr
-  confirmed_utxos =  MempoolApiClient
-    .get_utxo(addr)
-    .filter { |utx| utx[:status][:confirmed] }
-  satoshies_balance = confirmed_utxos.reduce(0) { |sum, utx| sum + utx[:value] }
+  from_addr = Bitcoin::Descriptor::Wpkh
+    .new(Bitcoin::Secp256k1::Ruby.generate_pubkey(privkey))
+    .to_script
+    .to_addr
+  from_addr_confirmed_utxos = MempoolApiClient
+    .get_utxo(from_addr)
+    .filter { |utxo| utxo[:status][:confirmed] }
+  from_addr_satoshies = from_addr_confirmed_utxos.reduce(0) { |sum, utxo| sum + utxo[:value] }
 
-  satoshies_amount = btc2satoshi(btc_amount) + CO
-  if satoshies_balance < btc2satoshi(btc_amount) + COMISSION_SATOSHIES
+  to_addr_satoshies = btc2satoshi(btc_amount)
+  if from_addr_satoshies < to_addr_satoshies + COMISSION_SATOSHIES
     puts 'Not enough coins'
     return
   end
 
-  my_vouts = confirmed_utxos.each_with_object({}) do |utxo, h|
-    tx = MempoolApiClient.get_transaction(utxo[:txid])
-    h[utxo[:txid]] = tx[:vout].filter { |vout| vout[:scriptpubkey_address] == addr }
+  tx = Bitcoin::Tx.new
+  tx.out << Bitcoin::TxOut.new(
+    value: to_addr_satoshies,
+    script_pubkey: Bitcoin::Script.parse_from_addr(to_addr)
+  )
+  change = from_addr_satoshies - to_addr_satoshies - COMISSION_SATOSHIES
+  if change != 0
+    tx.out << Bitcoin::TxOut.new(
+      value: change,
+      script_pubkey: Bitcoin::Script.parse_from_addr(from_addr)
+    )
+  end
+  from_addr_confirmed_utxos.each do |utxo_attrs|
+    utxo = MempoolApiClient.get_transaction(utxo_attrs[:txid])
+    vout_idxs = utxo[:vout]
+    utxo[:vout]
+      .each_with_index
+      .filter { |vout, idx| vout[:scriptpubkey_address] == from_addr }
+      .map { |_vout, idx| idx }
+      .each_with_index do |vout_idx, input_idx|
+        tx.in << Bitcoin::TxIn.new(out_point: Bitcoin::OutPoint.from_txid(utxo[:txid], vout_idx))
+        script_pubkey = Bitcoin::Script.parse_from_payload(
+          utxo[:vout][vout_idx][:scriptpubkey].htb
+        )
+        sig_hash = tx.sighash_for_input(
+          input_idx,
+          script_pubkey,
+          sig_version: :witness_v0,
+          amount: utxo[:vout][vout_idx][:value]
+        )
+        btc_key = Bitcoin::Key.new(priv_key: privkey)
+        signature = btc_key.sign(sig_hash) + [Bitcoin::SIGHASH_TYPE[:all]].pack('C')
+        tx.in[input_idx].script_witness.stack << signature << btc_key.pubkey.htb
+      end
   end
 
-  puts my_vouts
-  puts "Sending #{btc_amount} to #{receiver_addr}"
+  puts tx.to_payload.unpack('H*')
+  # puts "Sending #{btc_amount} to #{receiver_addr}"
 end
 
 # TODO: нормально парсить аргументы
